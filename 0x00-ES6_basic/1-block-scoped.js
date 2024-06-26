44a896cac7a3d161b3ea733ffceb717531205527
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-06-26 20:00:17 +0100

    f

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,3 +1,5 @@
+// 1-block-scoped.js
+
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
@@ -9,3 +11,4 @@ export default function taskBlock(trueOrFalse) {
 
   return [task, task2];
 }
+
