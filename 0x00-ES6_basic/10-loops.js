672a54aa04380a1b1c018a610d8db73e3ed21f30
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-06-26 20:37:06 +0100

    g

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+// 10-loops.js
+
+export default function appendToEachArrayValue(array, appendString) {
+  for (let value of array) {
+    value = appendString + value;
+  }
+
+  return array;
+}
+
---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,10 @@
+// 11-createEmployeesObject.js
+
+export default function createEmployeesObject(departmentName, employees) {
+  const employeesObject = {
+    [departmentName]: [...employees],
+  };
+
+  return employeesObject;
+}
+
