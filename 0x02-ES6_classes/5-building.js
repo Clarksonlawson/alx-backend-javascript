9d8abe12047a8bcc1be75b4701fb915d520591a6
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-06-29 20:22:48 +0100

    fg

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+// 5-building.js
+
+export default class Building {
+    constructor(sqft) {
+        if (new.target === Building) {
+            throw new Error("Cannot instantiate Building directly");
+        }
+        this._sqft = sqft;
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+
+    evacuationWarningMessage() {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+}
+
