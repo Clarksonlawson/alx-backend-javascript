05042cdbdd2774f1d909fbef9857d7ab7d07b912
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-07-02 18:46:42 +0000

    fg

---
+++ 0x03-ES6_data_manipulation/5-main.js
@@ -0,0 +1,5 @@
+// 5-main.js
+import createInt8TypedArray from "./5-typed_arrays.js";
+
+console.log(createInt8TypedArray(10, 2, 89));
+
---
+++ 0x03-ES6_data_manipulation/9-main.js
@@ -0,0 +1,5 @@
+// 9-main.js
+import groceriesList from "./9-groceries_list.js";
+
+console.log(groceriesList());
+
