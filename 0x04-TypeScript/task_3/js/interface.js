57c2fac6b2acde1a2ea0369a50d2e8d64ba05587
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-07-03 19:43:15 +0100

    hi

---
+++ 0x04-TypeScript/task_3/js/interface.js
@@ -0,0 +1,12 @@
+// task_3/js/interface.ts
+
+// Define type RowID
+export type RowID = number;
+
+// Define interface RowElement
+export interface RowElement {
+    firstName: string;
+    lastName: string;
+    age?: number;
+}
+
