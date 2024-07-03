43bdacd20746014018f2227c75ddc4bea89a2b4f
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-07-03 19:34:14 +0100

    hk

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,23 +1,29 @@
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [propName: string]: any;
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
 }
 
-interface Directors extends Teacher {
-  numberOfReports: number;
+interface StudentInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
 
-console.log(director1);
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
 
