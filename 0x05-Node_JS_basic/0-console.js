9580c25f26941373c288736180db609b8baa7d7d
Awomu Clarkson <clarksonlawson1@gmail.com>
2024-08-19 18:26:07 +0100

    G

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,9 +1,15 @@
-// 0-console.js
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+// Arrow function
+const displayMessage = (message) => process.stdout.write(message);
 
-// Function to print a string to STDOUT
-function displayMessage(message) {
-    process.stdout.write(message + '\n');
-}
-
-// Export the function
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+// Default module export.
 module.exports = displayMessage;
