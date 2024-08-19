3ea6a5ed18d1ba28a0ba23b621f072f6d32e5419
Awomu Clarkson <clarksonlawson1@gmail.com>
2024-08-19 18:21:46 +0100

    f

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
+// 0-console.js
+
+// Function to print a string using STDOUT
+
 function displayMessage(helloMessage) {
     process.stdout.write(helloMessage +'\n');    
 }
 
+// Export the function
 module.exports = displayMessage;
