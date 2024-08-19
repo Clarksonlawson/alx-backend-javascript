85e741a8b2980a8c655e3a78141f460159cd303b
Awomu Clarkson <clarksonlawson1@gmail.com>
2024-08-19 18:52:24 +0100

    G

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,13 @@
+// Display the initial prompt
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+// Handle incoming data from stdin
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
+    // Output the user's name
+    process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
 });
 
+// Handle the end of stdin stream
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+    process.stdout.write('This important software is now closing\n');
 });
