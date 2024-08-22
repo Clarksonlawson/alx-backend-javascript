97ddb199668008c3f8158a83f135092fea9398fc
Awomu Clarkson <clarksonlawson1@gmail.com>
2024-08-22 19:29:58 +0100

    g

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,11 @@
+/**
+ * This is the app file js.
+ */
+class AppController {
+    static getHomepage(request, response) {
+      response.status(200).send('Hello Holberton School!');
+    }
+  }
+  
+  export default AppController;
+  module.exports = AppController;
